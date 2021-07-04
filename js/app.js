899468eec0643e5a7108c9c4041f13c767ea598a
zyeesi <zyeesi@gmail.com>
2021-07-03 22:24:18 -0700

    Added display options for about page

--- js/app.js
+++ js/app.js
@@ -3,5 +3,10 @@ const filter_btns = document.querySelectorAll(".filter-btn");
 filter_btns.forEach( btn => btn.addEventListener("click", () => {
     filter_btns.forEach(button => button.classList.remove("active"));
     btn.classList.add("active");
-    
+
+    filter_btns.forEach(button => $(button.dataset.filter).css('display', 'none'));
+
+    let filterValue = btn.dataset.filter;
+    $(filterValue).css('display', 'block');
 }));
+
