6ed1f9f1d8c7da213e0f9b260de5644d47c2b64c
zyeesi <zyeesi@gmail.com>
2021-07-05 10:32:45 -0700

    Finished footer and responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const filter_btns = document.querySelectorAll(".filter-btn");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
 filter_btns.forEach( btn => btn.addEventListener("click", () => {
     filter_btns.forEach(button => button.classList.remove("active"));
@@ -10,3 +13,18 @@ filter_btns.forEach( btn => btn.addEventListener("click", () => {
     $(filterValue).css('display', 'block');
 }));
 
+hamburger_menu.addEventListener("click", () => {
+    if(!navbar.classList.contains("open")){
+        navbar.classList.add("open");
+        document.body.classList.add("stop-scrolling");
+    } else {
+        closeMenu();
+    }
+});
+
+function closeMenu() {
+    navbar.classList.remove("open");
+    document.body.classList.remove("stop-scrolling");
+}
+
+links.forEach( link => link.addEventListener("click", () => closeMenu()));
